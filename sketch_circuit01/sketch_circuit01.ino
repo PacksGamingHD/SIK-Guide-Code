f3c80b455e1f060213625764dc7043474e731d11
PacksGamingHD <craftyswans@gmail.com>
2015-02-24 20:32:01 -0800

    Update sketch_circuit01.ino

--- sketch_circuit01/sketch_circuit01.ino
+++ sketch_circuit01/sketch_circuit01.ino
@@ -1,26 +1,107 @@
-int timeDelay[] = {500,1000,1350};
+/*
+HC-SR04 Ping distance sensor]
+VCC to arduino 5v GND to arduino GND
+Echo to Arduino pin 13 Trig to Arduino pin 12
+Red POS to Arduino pin 11
+Green POS to Arduino pin 10
+560 ohm resistor to both LED NEG and GRD power rail
+More info at: http://goo.gl/kJ8Gl
+Original code improvements to the Ping sketch sourced from Trollmaker.com
+Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
+*/
 
-void setup()
-{
-  pinMode(13, OUTPUT);
+#define trigPin 3
+#define echoPin 2
+#define led 9
+#define led2 10
+const int buzzerPin = 8;
+const int songLength = 1;
+char notes[] = "c"; // a space represents a rest
+int beats[] = {4};
+int tempo = 150;
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
 }
 
-void loop()
-{
-  int index;
-  for(index = 0; index <= 2; index++);
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance < 50) {  // This is where the LED On/Off happens
+    digitalWrite(led, HIGH); // When the Red condition is met, the Green LED should turn off
+  digitalWrite(led2, LOW);
+    int i, duration;
+  
+  for (i = 0; i < songLength; i++) // step through the song arrays
   {
-  turnLED(timeDelay[index]);
-  }
+    duration = beats[i] * tempo;  // length of note/rest in ms
+    
+    if (notes[i] == ' ')          // is this a rest? 
+    {
+      delay(duration);            // then pause for a moment
+    }
+    else                          // otherwise, play the note
+    {
+      tone(buzzerPin, frequency(notes[i]), duration);
+      delay(duration);            // wait for tone to finish
+    }
+    delay(tempo/10);              // brief pause between notes
 }
-
-void turnLED(int timeDelay)
+  }
+  else {
+    digitalWrite(led,LOW);
+    digitalWrite(led2,HIGH);
+  }
+  if (distance >= 400 || distance <= 200){
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  delay(500);
+}
+int frequency(char note) 
 {
-  digitalWrite(13, LOW);
+  // This function takes a note character (a-g), and returns the
+  // corresponding frequency in Hz for the tone() function.
   
-  delay(timeDelay);
+  int i;
+  const int numNotes = 8;  // number of notes we're storing
   
-  digitalWrite(13, HIGH);
+  // The following arrays hold the note characters and their
+  // corresponding frequencies. The last "C" note is uppercase
+  // to separate it from the first lowercase "c". If you want to
+  // add more notes, you'll need to use unique characters.
+
+  // For the "char" (character) type, we put single characters
+  // in single quotes.
+
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int frequencies[] = {262, 294, 330, 349, 392, 440, 494, 523};
   
-  delay(timeDelay);
+  // Now we'll search through the letters in the array, and if
+  // we find it, we'll return the frequency for that note.
+  
+  for (i = 0; i < numNotes; i++)  // Step through the notes
+  {
+    if (names[i] == note)         // Is this the one?
+    {
+      return(frequencies[i]);     // Yes! Return the frequency
+    }
+  }
+  return(0);  // We looked through everything and didn't find it,
+              // but we still need to return a value, so return 0.
 }
